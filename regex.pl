2c6bb7853bd90e2a422bfe6bbdae950d3d3e7f0a
u <u>
2025-04-10 09:39:45 +0300

    update

--- regex.pl
+++ regex.pl
@@ -1,6 +1,6 @@
 s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath2 \1/%{goihead}~g;
 
-my $text = "%{lua: print(rpm.expand('%version'):gsub('%..*', ''))}";
+my $text = "%{lua: local i = rpm.expand('%version'):gsub('%..*', ''); print(i)}";
 
 s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4\n%define goipath %{goipath2}/v$text~g;
 s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1\n~g;
