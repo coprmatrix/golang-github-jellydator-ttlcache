365eb58eb49f50ed29f8a4426671022e39a1fc0d
u <u>
2025-04-10 12:20:13 +0300

    update

--- regex.pl
+++ regex.pl
@@ -1,9 +1,9 @@
-s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath2 \1/%{goihead}~g;
+s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath \1/%{goihead}~g;
 
 my $text = "%{lua: local i = rpm.expand('%version'):gsub('%..*', ''); print(i)}";
 
-s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4\n%define goipath %{goipath2}/v$text~g;
+s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4~g;
 s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1\n~g;
 
 s~%gocheck~~g;
-s~Source:.*~%define scommit %{?commit}%{?!commit:v%{version}}\n%define stag %{?tag}%{?!tag:%scommit}\nSource: https://%{goipath2}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
+s~Source:.*~%define scommit %{?commit}%{?!commit:v%{version}}\n%define stag %{?tag}%{?!tag:%scommit}\nSource: https://%{goipath}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
